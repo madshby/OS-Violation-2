571b353e905f9c1e48457d82a253437fcefda992
Mads Harby <71691732+madshby@users.noreply.github.com>
2021-03-26 10:36:19 +0100

    stuff

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -7,6 +7,10 @@ public class Main {
     public static void main(String[] args) {
         BigInteger n = BigInteger.ONE;
         for(int i = 2; i > 0; i++) {
+            for(int j = 2; j > 0; j++){
+                n = n.multiply(BigInteger.valueOf(i));
+                System.out.println(n);
+            }
             n = n.multiply(BigInteger.valueOf(i));
             System.out.println(n);
         }
